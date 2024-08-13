af384b9864b31d6268e4d14782824a7a42cc30bb
Matthew Novelli <mattnovelli@gmail.com>
2024-08-13 15:38:38 -0500

    update to current

---
+++ IpDisplaysApiClientConfig.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mtd.Kiosk.LedUpdater.IpDisplaysApi;
+public class IpDisplaysApiClientConfig
+{
+	public const string CONFIG_SECTION_NAME = "IPDisplaysApiClient";
+
+	[Required, Range(1, int.MaxValue)]
+	public required int TimeoutMiliseconds { get; set; }
+}
